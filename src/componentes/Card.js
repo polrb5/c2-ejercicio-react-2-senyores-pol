6b9d208f3f2e0110992b845b513b77881090970c
Pol Reig <polreigbroto@gmail.com>
2021-06-11 16:58:36 +0200

    Datos de las cards rellenados

--- src/componentes/Card.js
+++ src/componentes/Card.js
@@ -1,5 +1,10 @@
 export const Card = (props) => {
   const { senyor } = props;
+  const getInicial = (nombre) => {
+    const partesNombre = nombre.split(" ");
+    const posicion = partesNombre[0].length > 2 ? 0 : 1;
+    return partesNombre[posicion].charAt(0).toUpperCase();
+  };
   return (
     <article className="senyor senyor-molde col-8 offset-2 mb-4">
       <div className="row">
@@ -10,22 +15,24 @@ export const Card = (props) => {
             width="214"
             alt=""
           />
-          <span className="inicial text-center rounded-circle"></span>
+          <span className="inicial text-center rounded-circle">
+            {getInicial(senyor.nombre)}
+          </span>
         </div>
         <div className="info col">
           <h2 className="nombre-senyor">{senyor.nombre}</h2>
           <ul className="datos list-unstyled mt-2">
             <li className="datos-profesion row">
               <span className="nombre-dato col-4">Profesión:</span>
-              <span className="valor-dato col"></span>
+              <span className="valor-dato col">{senyor.profesion}</span>
             </li>
             <li className="datos-estado row">
               <span className="nombre-dato col-4">Estado:</span>
-              <span className="valor-dato col"></span>
+              <span className="valor-dato col">{senyor.estado}</span>
             </li>
             <li className="datos-twitter row">
               <span className="nombre-dato col-4">Twitter:</span>
-              <span className="valor-dato col"></span>
+              <span className="valor-dato col">{senyor.twitter}</span>
             </li>
           </ul>
         </div>
