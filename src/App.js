5b0f2f67b3851d058289831f48b93c815bfae5d9
Pol Reig <polreigbroto@gmail.com>
2021-06-11 13:46:35 +0200

    Componentes creados y carpeta img añadida

--- src/App.js
+++ src/App.js
@@ -1,4 +1,15 @@
+import { Card } from "./componentes/Card";
+import { Header } from "./componentes/Header";
+import { senyoresAPI } from "./senyores";
+
 function App() {
-  return;
+  return (
+    <div className="contenedor-general container-xl">
+      <Header />
+      <main className="principal mt-2 row">
+        <Card />
+      </main>
+    </div>
+  );
 }
 export default App;
